e01f13642043d2c124e1cf05143719418ffe0a09
Alberto Puliga <albi.puliga@gmail.com>
2023-11-15 23:25:32 +0100

    refactoring

---
+++ dbs_app/routers.py
@@ -0,0 +1,19 @@
+class MongoDBRouter:
+    """
+    A database router to route certain models to MongoDB.
+    """
+
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label == 'dbs_app':
+            return 'mongodb'
+        return None
+
+    def db_for_write(self, model, **hints):
+        if model._meta.app_label == 'dbs_app':
+            return 'mongodb'
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if db == 'mongodb':
+            return False  # Don't allow migrations for MongoDB
+        return None
