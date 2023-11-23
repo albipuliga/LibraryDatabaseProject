97fcfe5ce7e82d5418e38f25b665db2a5535a667
Alberto Puliga <albi.puliga@gmail.com>
2023-11-23 15:57:46 +0100

    implemented webscraping though isbn csv

--- dbs_app/models.py
+++ dbs_app/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
-    bio = models.TextField()
-
+    name = models.CharField(max_length=255)
+    key = models.CharField(max_length=255, default="")
     # class Meta:
     #     # Set the database alias to 'mongodb'
     #     database = 'mongodb'
@@ -14,9 +13,12 @@ class Author(models.Model):
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=100, default="")
+    title = models.CharField(max_length=255, default="")
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    isbn = models.CharField(max_length=255, default="")
+    publishers = models.CharField(max_length=255, default="")
+    number_of_pages = models.IntegerField(default=0)
+    languages = models.CharField(max_length=255, default="")
 
     # class Meta:
     #     # Set the database alias to 'mongodb'
@@ -24,29 +26,3 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Borrower(models.Model):
-    name = models.CharField(max_length=100, default="")
-    email = models.EmailField()
-
-    # class Meta:
-    #     # Set the database alias to 'mongodb'
-    #     database = 'mongodb'
-
-    def __str__(self):
-        return self.name
-
-
-class Loan(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    borrower = models.ForeignKey(Borrower, on_delete=models.CASCADE)
-    date_borrowed = models.DateField()
-    date_returned = models.DateField(null=True, blank=True)
-
-    # class Meta:
-    #     # Set the database alias to 'mongodb'
-    #     database = 'mongodb'
-
-    def __str__(self):
-        return f"{self.book} borrowed by {self.borrower}"
---
+++ webscraping.py
@@ -0,0 +1,69 @@
+import os
+import django
+import requests
+import pandas as pd
+from datetime import datetime
+
+# Set up the Django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dbs.settings")
+django.setup()
+
+from dbs_app.models import Book, Author
+
+
+def get_isbn_list(file_path):
+    # Read the CSV file
+    df = pd.read_csv(file_path)
+
+    # Extract the ISBN column into a list
+    isbn_list = df["isbn"].tolist()
+
+    return isbn_list
+
+
+def get_book_details_by_isbn(isbn_list):
+    for isbn in isbn_list:
+        url = f"https://openlibrary.org/api/books?bibkeys=ISBN:{isbn}&format=json&jscmd=data"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            book_data = response.json().get(f"ISBN:{isbn}", {})
+
+            title = book_data.get("title")
+            authors = book_data.get("authors", [])
+            publishers = book_data.get("publishers", [])
+            number_of_pages = book_data.get("number_of_pages")
+            languages = book_data.get("languages", [])
+
+            if title and authors:
+                # Create or update the author in your database
+                author_name, key = (
+                    authors[0].get("name"),
+                    authors[0].get("key") or "",
+                )  # Assuming the first author
+                author, created = Author.objects.get_or_create(
+                    name=author_name, key=key
+                )
+                langs = languages[0].get("key").split("/") if languages else []
+                # Create or update the book in your database
+                book, created = Book.objects.update_or_create(
+                    title=title,
+                    defaults={
+                        "author": author,
+                        "isbn": isbn,
+                        "publishers": ", ".join(p.get("name", "") for p in publishers),
+                        "number_of_pages": number_of_pages or 0,
+                        "languages": langs
+                    },
+                )
+                print(f'{"Created" if created else "Updated"}: {book.title}')
+            else:
+                print(f"No detailed information found for ISBN {isbn}")
+        else:
+            print(
+                f"Failed to retrieve data for ISBN {isbn}. Status code: {response.status_code}"
+            )
+
+
+isbn_list = get_isbn_list("books.csv")
+get_book_details_by_isbn(isbn_list)
